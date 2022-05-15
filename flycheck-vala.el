ebf1aac75376bdebf1636241cddd804d4de611b8
Junker <1144095+Junker@users.noreply.github.com>
2022-05-16 00:07:14 +0700

    add package header

--- flycheck-vala.el
+++ flycheck-vala.el
@@ -1,3 +1,27 @@
+;;; flycheck-vala.el --- Flycheck checker for Vala
+
+;; Version: 0.1.0
+;; URL: https://github.com/Junker/flycheck-vala
+;; Package-Requires: ((flycheck "0.22"))
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Flycheck checker for Vala
+
+;;; Code:
+
 (require 'flycheck)
 
 (defun get-value-from-comments (marker-string line-limit)
@@ -88,3 +112,5 @@ See URL `https://wiki.gnome.org/Projects/Vala'."
      (add-to-list 'flycheck-checkers 'vala-check)))
 
 (provide 'flycheck-vala)
+
+;;; flycheck-vala.el ends here
